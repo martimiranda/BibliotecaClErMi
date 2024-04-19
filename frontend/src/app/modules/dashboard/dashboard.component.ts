f5155feb1631805c87c8523d7e57905f4de88a1a
cl6mh7 <claudiamoyamo@gmail.com>
2024-04-19 19:13:19 +0200

    feat(dashboard): Created dashboard

--- frontend/src/app/modules/dashboard/dashboard.component.ts
+++ frontend/src/app/modules/dashboard/dashboard.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+            ButtonModule,
+            AvatarModule,
+            AvatarGroupModule
+          ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
