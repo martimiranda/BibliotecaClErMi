4229f68a705dd7cf8d19dc59c0d61370df290e22
martimiranda <martimiranda2356@gmail.com>
2024-04-19 19:23:30 +0200

    last

--- biblioteca/biblioApp/models.py
+++ biblioteca/biblioApp/models.py
@@ -2,77 +2,142 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Role(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50 , verbose_name="Nom del rol")
+
+    class Meta:
+        verbose_name = "Rol"
+        verbose_name_plural = "Rols"
 
     def __str__(self):
         return self.name
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.ForeignKey(Role, on_delete=models.CASCADE)
-    date_of_birth = models.DateField()
-    center = models.CharField(max_length=100)
-    cycle = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='user_images/', blank=True, null=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE , verbose_name="Usuari")
+    name = models.CharField(max_length=100 , verbose_name="Nom")
+    surname = models.CharField(max_length=100 , verbose_name="Cognom")
+    surname2 = models.CharField(max_length=100 , verbose_name="Segon cognom (opcional)", blank=True, null=True)
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, verbose_name="Rol d'usuari")
+    date_of_birth = models.DateField(verbose_name="Data de naixement")
+    center = models.CharField(max_length=100 , verbose_name="Centre al que pertany")
+    cycle = models.CharField(max_length=100 , verbose_name="Curs al que pertany")
+    image = models.ImageField(upload_to='user_images/', blank=True, null=True , verbose_name="Imatge de perfil")
+
+    class Meta:
+        verbose_name = "Perfil d'usuari"
+        verbose_name_plural = "Perfils d'usuari"
 
     def __str__(self):
         return self.user.username
 
-class GlobalOption(models.Model):
-    option_name = models.CharField(max_length=100)
-    option_value = models.CharField(max_length=255)
 
-    def __str__(self):
-        return self.option_name
 
-class ItemType(models.Model):
-    name = models.CharField(max_length=100)
+class Item(models.Model):
+    title = models.CharField(max_length=100 , verbose_name="Títol del item")    
+    material_type = models.TextField(max_length=100, verbose_name="Material del que es composa")  
+    signature = models.CharField(max_length=100, verbose_name="Signatura")   
 
-    def __str__(self):
-        return self.name
-
-class Catalog(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-
-    def __str__(self):
-        return self.name
-
-class CatalogItem(models.Model):
-    catalog = models.ForeignKey(Catalog, on_delete=models.CASCADE)
-    item_type = models.ForeignKey(ItemType, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    publication_year = models.PositiveIntegerField()
-    quantity = models.PositiveIntegerField()
+    class Meta:
+        verbose_name = "Item del catàlog"
+        verbose_name_plural = "Items dels catàlogs"
 
     def __str__(self):
         return self.title
 
+
+class Book (Item):
+    author = models.CharField(max_length=100 , verbose_name="Autor del item") 
+    edition_date = models.DateField(verbose_name="Data de l'edició")
+    CDU = models.TextField(max_length=100)
+    ISBN = models.TextField(max_length=13)
+    publisher = models.CharField(max_length=100, verbose_name="Editorial")
+    colection = models.CharField(max_length=100, verbose_name="Col·lecció")
+    pages = models.PositiveIntegerField(verbose_name="Pàgines")
+
+    class Meta:
+        verbose_name = "Llibre"
+        verbose_name_plural = "Llibres"
+
+class CD(Item):
+    author = models.CharField(max_length=100 , verbose_name="Autor del item") 
+    edition_date = models.DateField(verbose_name="Data de l'edició")
+    discography = models.CharField(max_length=100, verbose_name="Discografía")
+    style = models.CharField(max_length=100, verbose_name="Estil")
+    duration = models.TimeField(verbose_name="Duració")
+    
+
+    class Meta:
+        verbose_name = "CD"
+        verbose_name_plural = "CD's"
+
+    def __str__(self):
+        return self.name
+
+class Dispositive(Item):
+    brand = models.CharField(max_length=100, verbose_name="Marca del dispositiu")
+    dispo_type = models.CharField(max_length=100, verbose_name="Tipus de dispositiu")
+
+    class Meta:
+        verbose_name = "Dispositiu"
+        verbose_name_plural = "Dispositius"
+
+    def __str__(self):
+        return self.name
+
 class ItemCopy(models.Model):
-    catalog_item = models.ForeignKey(CatalogItem, on_delete=models.CASCADE)
-    status = models.CharField(max_length=20, default='Available')
+    item = models.ForeignKey(Item, on_delete=models.CASCADE , verbose_name="Item")
+    status = models.CharField(max_length=20, default='Available' , verbose_name="Disponibilitat")
+    id_copy = models.CharField(max_length=100, verbose_name="Id del exemplar") 
 
-class Reservation(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    copy = models.ForeignKey(ItemCopy, on_delete=models.CASCADE)
-    reservation_date = models.DateField(auto_now_add=True)
+    class Meta:
+        verbose_name = "Exemplar del item"
+        verbose_name_plural = "Exemplars dels items"
 
-class Loan(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    copy = models.ForeignKey(ItemCopy, on_delete=models.CASCADE)
-    loan_date = models.DateField(auto_now_add=True)
-    return_date = models.DateField(null=True, blank=True)
+    def __str__(self):
+        return self.name
+ 
 
-class Request(models.Model):
-    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    request_date = models.DateField(auto_now_add=True)
-    request_text = models.TextField()
+class Reservation(models.Model): #RESERVA
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name="Usuari")
+    copy = models.ForeignKey(ItemCopy, on_delete=models.CASCADE, verbose_name="Exemplar")
+    reservation_date = models.DateField(auto_now_add=True , verbose_name="Data de la reserva")
+
+    class Meta:
+        verbose_name = "Reserva"
+        verbose_name_plural = "Reserves"
+
+    def __str__(self):
+        return self.name
+
+class Loan(models.Model): #PRESTEC
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name="Usuari")
+    copy = models.ForeignKey(ItemCopy, on_delete=models.CASCADE, verbose_name="Exemplar")
+    loan_date = models.DateField(auto_now_add=True, verbose_name="Data de la solicitud del prèstec")
+    return_date = models.DateField(null=True, blank=True, verbose_name="Data del final del prèstec")
+
+    class Meta:
+        verbose_name = "Prèstec"
+        verbose_name_plural = "Prèstecs"
+
+    def __str__(self):
+        return self.name
+
+class Request(models.Model): #PETICIO
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, verbose_name="Usuari")
+    request_date = models.DateField(auto_now_add=True, verbose_name="Data de la petició")
+    request_text = models.TextField(verbose_name="Descripció de la petició")
 
 class Log(models.Model):
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     action_date = models.DateTimeField(auto_now_add=True)
     action_description = models.TextField()
+
+    class Meta:
+        verbose_name = "Log"
+        verbose_name_plural = "Logs"
+    
+    def __str__(self):
+        return self.name
+
 
 
 
